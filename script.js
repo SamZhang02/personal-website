7a93cc89bbd84fba338b80f87057793a53e24566
SamZhang02 <sam.zijing.zhang@gmail.com>
2022-11-06 15:11:05 -0500

    improve code cleaniness

--- script.js
+++ script.js
@@ -2,29 +2,7 @@ const body = document.querySelector('body');
 const titles = document.querySelector('.heading').children;
 let width = document.body.clientWidth;
 
-width = document.body.clientWidth;
-if (width <= 697){
-    for (i=0; i < titles.length; i++){
-        if (titles[i].tagName === 'P'){ 
-            titles[i].style.textAlign = 'center';
-        }
-        else {
-            titles[i].style.justifyContent = 'center';
-        }
-    }
-} 
-else {
-    for (i=0; i < titles.length; i++){
-        if (titles[i].tagName === 'P'){ 
-            titles[i].style.textAlign = 'left';
-        }
-        else {
-            titles[i].style.justifyContent = 'left';
-        }
-    }
-}
-
-document.body.onresize = function(){
+function resize(){
     width = document.body.clientWidth;
     if (width <= 697){
         for (i=0; i < titles.length; i++){
@@ -47,4 +25,5 @@ document.body.onresize = function(){
         }
     }
 }
-
+resize()
+document.body.onresize = resize
